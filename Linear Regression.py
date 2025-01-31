e7a0a9d8ce05ac67244eb14cd0815218e401b1dd
DhidseSrushti123 <Srushtidhidse@gmail.com>
2025-01-31 10:28:20 +0530

    Add files via upload

---
+++ Linear Regression.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+# In[3]:
+
+
+data=pd.read_csv("NewspaperData.csv")
+data
+
+
+# In[ ]:
+
+
+
+
