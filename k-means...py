90704d4e501c760f1bc1900cc7af1c964fa502c0
DhidseSrushti123 <Srushtidhidse@gmail.com>
2025-02-12 11:40:37 +0530

    Add files via upload

---
+++ k-means...py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np 
+import pandas as pd
+import matplotlib.pyplot as plt
+import warnings 
+warnings.filterwarnings("ignore")
+from sklearn.cluster import KMeans
+
+
+# # Clustering -Divide the universities in to group(clusters)
+# 
+
+# In[2]:
+
+
+Univ = pd.read_csv("Universities.csv")
+Univ
+
+
+# In[3]:
+
+
+Univ.describe()
+
+
+# # Standardization of data
+
+# In[4]:
+
+
+Univ1 = Univ.iloc[:,1:]
+
+
+# In[5]:
+
+
+Univ1
+
+
+# In[6]:
+
+
+cols = Univ1.columns
+
+
+# In[7]:
+
+
+# Standardisation function
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+scaled_Univ_df = pd.DataFrame(scaler.fit_transform(Univ1),columns = cols)
+scaled_Univ_df
+
+
+# In[8]:
+
+
+from sklearn.cluster import KMeans
+clusters_new = KMeans(3, random_state=0)
+clusters_new.fit(scaled_Univ_df)
+
+
+# In[9]:
+
+
+clusters_new.labels_
+
+
+# In[10]:
+
+
+set(clusters_new.labels_)
+
+
+# In[11]:
+
+
+Univ['clusterid_new'] = clusters_new.labels_
+
+
+# In[12]:
+
+
+Univ
+
+
+# In[13]:
+
+
+Univ.sort_values(by = "clusterid_new")
+
+
+# In[14]:
+
+
+Univ.iloc[:,1:].groupby("clusterid_new").mean()
+
+
+# # Observation 
+# - cluster 2 apperas to be the top rated universities cluster as the cut off score,Top 10,SFRatio paparmeter mean values are highest
+# - cluster 1 appears to occupy the middle level rated universities
+# - cluster 0 comes as the lower rated universities
+
+# In[15]:
+
+
+Univ[Univ['clusterid_new']==0]
+
+
+# # Finding optimal k values using elbow plot
+
+# In[16]:
+
+
+wcss =[]
+for i in range(1, 20):
+    
+    kmeans = KMeans(n_clusters=i,random_state=0)
+    kmeans.fit(scaled_Univ_df)
+    
+    wcss.append(kmeans.inertia_)
+print(wcss)
+plt.plot(range(1, 20),wcss)
+plt.title('Elbow Method')
+plt.xlabel('Number of clusters')
+plt.ylabel('WCSS')
+plt.show()
+
+
+# In[17]:
+
+
+Univ.info()
+
+
+# # Find the quality of clusters
+
+# In[18]:
+
+
+#Quality of clusters is expressed in terms
+#of silhoutte score
+from sklearn.metrics import silhouette_score
+score =silhouette_score(scaled_Univ_df, \
+        clusters_new.labels_ , metric='euclidean')
+score
+
